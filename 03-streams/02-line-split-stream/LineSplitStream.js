901e62256177a4d3cb1b079fbca432be46a14a07
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-01-24 16:03:45 +0200

    add solution for 3-module 3-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,20 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.encoding = options.encoding;
+    this.line = "";
   }
 
   _transform(chunk, encoding, callback) {
+    const str = chunk.toString(this.encoding);
+    this.line = str.split(`${os.EOL}`).filter(string => !!string);
+
+    callback(null, this.line);
   }
 
   _flush(callback) {
+    
+    callback();
   }
 }
 
--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,49 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
+
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      const stream = fs.createReadStream(filepath);
+      const pathNameSplit = url.pathname.split("/").filter(elem => elem !== "");
+
+      const body = [];
+      stream
+        .on('data', (chunk) => {
+          body.push(chunk);
+        })
+
+        .on('end', () => {
+          res.statusCode = 200;
+          res.end(Buffer.concat(body));
+        })
+
+        .on('error', () => {
+          if (pathNameSplit.length > 1) {
+            res.statusCode = 400;
+            res.end('Bad Request');
+          } else if (pathNameSplit.length === 1) {
+            res.statusCode = 404;
+            res.end('File Does Not Exist');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          }
+        })
+
+        .on('open', () => { })
+
+        .on('close', () => { });
 
       break;
 
