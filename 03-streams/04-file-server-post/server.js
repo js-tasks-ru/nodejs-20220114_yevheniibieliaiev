0ece8f3985ccb892de5576d412308cfa940a3191
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-01-28 14:00:43 +0200

    changes for solution for 3-module 4-task

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -6,47 +6,57 @@ const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
-server.on('request', async (req, res) => {
+server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Bad Request');
+  }
 
   switch (req.method) {
     case 'POST':
-      const pathNameSplit = url.pathname.split("/").filter(elem => elem !== "");
-      const limitStream = new LimitSizeStream({ limit: 1e6, readableObjectMode: true })
-      const outStream = fs.createWriteStream(filepath, { flags: 'wx+' });
+      const limitStream = new LimitSizeStream({ limit: 1e6 });
+      limitStream.on('error', (err) => {
+        if (err.code = 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          outStream.destroy();
+          limitStream.destroy();
+          fs.unlink(filepath, (err) => {
+            err ? console.log(err) : console.log('deleted');
+          });
+          res.end('Request Entity Too Large');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        }
 
-      req
-        .pipe(limitStream)
-        .on('error', (err) => {
-          if (err.code === 'LIMIT_EXCEEDED') {
-            res.statusCode = 413;
-            res.end(err.message);
-          }
-        })
-        .pipe(outStream)
-        .on('error', (err) => {
-          if (pathNameSplit.length > 1) {
-            res.statusCode = 400;
-            res.end('Bad Request');
-          } else if (err.code === 'EEXIST') {
-            res.statusCode = 409;
-            res.end('File With a Given Name Exists');
-          } else {
-            res.statusCode = 500;
-            res.end('Internal Server Error');
-          }
-        })
-        .on('finish', () => {
-          res.statusCode = 201;
-          res.end('File Has Been Created');
-        })
+      })
+
+      const outStream = fs.createWriteStream(filepath, { flags: 'wx+' });
+      outStream.on('error', (err) => {
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File With a Given Name Exists');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        }
+      });
 
       req.on('aborted', () => {
         outStream.destroy();
+        limitStream.destroy();
         fs.unlink(filepath, () => { });
+      })
+
+      req.pipe(limitStream).pipe(outStream);
+
+      outStream.on('finish', () => {
+        res.statusCode = 201;
+        res.end('File Has Been Created');
       });
 
       break;
