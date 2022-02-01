45432631ec0c6d350fb0dcf2431dca80e460f249
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-02-01 13:34:51 +0200

    add solution for 5-module 1-task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -1,17 +1,26 @@
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
+const events = require('events');//не сразу я пришел к мысли использовать EventEmitter :)
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const eventEmitter = new events.EventEmitter();
 
 router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await new Promise(resolve => eventEmitter.once('userMessage', msg => resolve(msg)));
+  ctx.status = 200;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const msg = ctx.request.body.message;
+  if (msg) {
+    await new Promise(resolve => resolve(eventEmitter.emit('userMessage', msg)));
+    ctx.status = 200;
+  }
 });
 
 app.use(router.routes());
