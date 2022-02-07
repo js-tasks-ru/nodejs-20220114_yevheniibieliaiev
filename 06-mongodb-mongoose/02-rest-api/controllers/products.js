3f7ca968631a9449fcdf08966a9805dd9976c7b3
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-02-08 00:08:50 +0200

    add solution for 6-module 1,2-tasks

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,34 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+//this function works with results of queries for productsBySubcategory & productList
+function queryResult(res) {
+  return res ? res.map(elem => mapProduct(elem)) : [];
+}
+
+//isValidId & queryByIdResult for productById
+//id validation
+const isValidId = id => ObjectId.isValid(id) ? String(new ObjectId(id) === id) ? true : false : false;
+//queryById
+function queryByIdResult(res, ctx) {
+  res ? ctx.body = { product: mapProduct(res) } : ctx.status = 404;
+}
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const {subcategory} = ctx.query;
+  const { subcategory } = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  ctx.body = { products: queryResult(await Product.find({ subcategory })) };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  ctx.body = { products: queryResult(await Product.find()) };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const idx = ctx.request.url.split('/')[3];
+  isValidId(idx) ? queryByIdResult(await Product.findOne({ _id: idx }), ctx) : ctx.status = 400;
 };
 
