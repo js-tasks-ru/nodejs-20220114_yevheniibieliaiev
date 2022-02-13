4008fa1dae3380199664052a0630570217460f9c
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-02-13 22:53:32 +0200

    add solution for 7-module 1,2-task

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,29 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  //1. Убедиться в том, что поле `email` передано.
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  try {
+    //2. Попытаться найти пользователя по переданному `email`.
+    const user = await User.findOne({ email });
+
+    //3. Если пользователь есть - аутентифицировать его (передав в коллбек стратегии `done` объект 
+    //пользователя).
+    if (user) {
+      return done(null, user);
+    } else {
+      //4. Если пользователя нет - создать для него новый документ и аутентифицировать.
+      const candidate = new User({ email, displayName });
+      await candidate.save();
+
+      const newUser = await User.findOne({ email });
+      return done(null, newUser);
+    }
+  } catch (err) {
+    return done(err);
+  }
 };
